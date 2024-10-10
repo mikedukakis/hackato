c8a15bedd644c907c2b7db59e170cae05715b787
Ignasi Moya <imoyaf@gmail.com>
2024-10-10 11:37:01 +0200

    Add swagger annotations for controllers

--- hackato/src/main/java/imf/hackato/controller/ActivityController.java
+++ hackato/src/main/java/imf/hackato/controller/ActivityController.java
@@ -2,11 +2,16 @@ package imf.hackato.controller;
 
 import imf.hackato.entity.Activity;
 import imf.hackato.service.ActivityService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import lombok.Data;
 import org.springframework.web.bind.annotation.*;
 import reactor.core.publisher.Mono;
 
+@Data
 @RestController
 @RequestMapping("/appActivities/activity")
+@Tag(name = "Activity", description = "Operations related to activity management")
 public class ActivityController {
     private final ActivityService activityService;
 
@@ -14,26 +19,31 @@ public class ActivityController {
         this.activityService = activityService;
     }
 
+    @Operation(summary = "Create activity", description = "Creates a new activity.")
     @PostMapping
     public Mono<Activity> createActivity(@RequestBody Activity activity){
         return activityService.createActivity(activity);
     }
 
+    @Operation(summary = "Update activity", description = "Updates an existing activity, located with Id.")
     @PutMapping("/{id}")
     public Mono<Activity> updateActivity(@PathVariable String id, Activity activity) {
         return activityService.updateActivity(id, activity);
     }
 
+    @Operation(summary = "Find activity", description = "Finds an existing activity, located with Id.")
     @GetMapping("/{id}")
     public Mono<Activity> findActivity(String id) {
         return activityService.findActivityById(id);
     }
 
+    @Operation(summary = "Sign-up user", description = "Signs a user (via user Id) up in the activity with a certain activity Id")
     @PutMapping("/{id}/signUp")
     public Mono<Activity> signUserUp(@PathVariable String activityId, @RequestBody String userId) {
         return activityService.addUserToActivity(activityId, userId);
     }
 
+    @Operation(summary = "Delete activity", description = "Deletes an existing activity, located with Id.")
     @DeleteMapping("/{id}")
     public Mono<Void> deleteActivity(@PathVariable String id) {
         return activityService.deleteActivity(id);
--- hackato/src/main/java/imf/hackato/controller/UserController.java
+++ hackato/src/main/java/imf/hackato/controller/UserController.java
@@ -2,11 +2,16 @@ package imf.hackato.controller;
 
 import imf.hackato.entity.User;
 import imf.hackato.service.UserService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import lombok.Data;
 import org.springframework.web.bind.annotation.*;
 import reactor.core.publisher.Mono;
 
+@Data
 @RestController
 @RequestMapping("/appActivities/users")
+@Tag(name = "User", description = "Operations related to user management")
 public class UserController {
     private final UserService userService;
 
@@ -14,21 +19,25 @@ public class UserController {
         this.userService = userService;
     }
 
+    @Operation(summary = "Create user", description = "Creates a new user.")
     @PostMapping("/user")
     public Mono<User> createUser(@RequestBody User user) {
         return userService.createUser(user);
     }
 
+    @Operation(summary = "Update user", description = "Updates an existing user, located with Id.")
     @PutMapping("/{id}")
     public Mono<User> updateUser(@PathVariable String id, @RequestBody User user) {
         return userService.updateUser(id, user);
     }
 
+    @Operation(summary = "Find user", description = "Finds an existing user, located with Id.")
     @GetMapping("/{id}")
     public Mono<User> findUser(@PathVariable String id) {
         return userService.findUserById(id);
     }
 
+    @Operation(summary = "Delete user", description = "Deletes an existing user, located with Id.")
     @DeleteMapping("/{id}")
     public Mono<Void> deleteUser(@PathVariable String id) {
         return userService.deleteUser(id);
