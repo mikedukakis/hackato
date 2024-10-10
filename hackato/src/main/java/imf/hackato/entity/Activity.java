0c911482c67c089bb27e766f8a0e10c9e5d68e9d
Ignasi Moya <imoyaf@gmail.com>
2024-10-10 09:39:18 +0200

    Cfreate attributes for User and Activity

--- hackato/src/main/java/imf/hackato/entity/Activity.java
+++ hackato/src/main/java/imf/hackato/entity/Activity.java
@@ -1,4 +1,21 @@
 package imf.hackato.entity;
 
+import lombok.Data;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Data
 public class Activity {
+    private String name;
+    private List<User> participants;
+    private short maxCapacity;
+    private String description;
+
+    public Activity(String name, short maxCapacity, String description) {
+        this.name = name;
+        this.participants = new ArrayList<>();
+        this.maxCapacity = maxCapacity;
+        this.description = description;
+    }
 }
--- hackato/src/main/java/imf/hackato/entity/User.java
+++ hackato/src/main/java/imf/hackato/entity/User.java
@@ -1,4 +1,14 @@
 package imf.hackato.entity;
 
+import lombok.Data;
+
+@Data
 public class User {
+    private String name;
+    private String lname;
+    private byte age;
+    private String email;
+    private String telephone;
+
+
 }
