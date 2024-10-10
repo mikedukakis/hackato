96742da97144c86ec02a1d257bd57606a81611ce
Ignasi Moya <imoyaf@gmail.com>
2024-10-10 11:41:06 +0200

    Add annotation for repositories

--- hackato/src/main/java/imf/hackato/repository/ActivityRepository.java
+++ hackato/src/main/java/imf/hackato/repository/ActivityRepository.java
@@ -2,7 +2,8 @@ package imf.hackato.repository;
 
 import imf.hackato.entity.Activity;
 import org.springframework.data.mongodb.repository.ReactiveMongoRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface ActivityRepository extends ReactiveMongoRepository<Activity, String> {
-
 }
--- hackato/src/main/java/imf/hackato/repository/UserRepository.java
+++ hackato/src/main/java/imf/hackato/repository/UserRepository.java
@@ -2,7 +2,8 @@ package imf.hackato.repository;
 
 import imf.hackato.entity.User;
 import org.springframework.data.mongodb.repository.ReactiveMongoRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface UserRepository extends ReactiveMongoRepository<User, String> {
-
 }
