cbcf9a419c244859da57ce39ee98d9e385568dcf
Ignasi Moya <imoyaf@gmail.com>
2024-10-10 08:31:11 +0200

    Initialise project

---
+++ hackato/src/main/java/imf/hackato/HackatoApplication.java
@@ -0,0 +1,13 @@
+package imf.hackato;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HackatoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HackatoApplication.class, args);
+	}
+
+}
---
+++ hackato/src/test/java/imf/hackato/HackatoApplicationTests.java
@@ -0,0 +1,13 @@
+package imf.hackato;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HackatoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
